a247003c4418253f7e7ccdd73c9e406e65b8c9ee
nwolfson <86404629+nwolfson@users.noreply.github.com>
2021-07-14 16:22:14 +0300

    Delete nidmm directory

--- nidmm/ac.py
+++
@@ -1,6 +0,0 @@
-import nidmm
-with nidmm.Session("Dev1") as session:
-    session.configure_measurement_digits(nidmm.Function.AC_VOLTS, 10, 5.5)
-    print("Measured Volts: " + str(session.read())) * 100
-    session.configure_measurement_digits(nidmm.Function.AC_CURRENT, 1, 5.5)
-    print("Measured Current: " + str(session.read())) * 100
