0865c9963d8e11a19ed0e85562930185dd50c9b0
nwolfson <86404629+nwolfson@users.noreply.github.com>
2021-07-29 17:46:47 +0300

    Delete strucc.py

--- strucc.py
+++
@@ -1,7 +0,0 @@
-import struct
-file = open('1.pof', 'rb')
-file_data = bytearray(file.read())
-f = struct.unpack_from('<hl', file_data, 0xc)
-f1 = struct.unpack_from('<2l', file_data, 0x76)
-f2 = struct.unpack_from('<hl', file_data, 0x74)
-print(f)
